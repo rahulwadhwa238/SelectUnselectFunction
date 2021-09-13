083151e068f642e970e0f8d3c05f3dcfcf2f2c1a
Rahul Wadhwa <rahulwadhwa238@gmail.com>
2021-09-13 13:46:55 +0530

    Create function.js

---
+++ function.js
@@ -0,0 +1,12 @@
+const [selectedItemList, setSelectedItemList] = useState([])
+
+const selectUnselectItem = (item) => {
+        let selectedItems = selectedItemList
+        if (selectedItems.find(x => x.id == item.id)) {
+            selectedItems = selectedItems.filter(x => x.id != item.id)
+        }
+        else {
+            selectedItems.push(item)
+        }
+        setSelectedItemList(selectedItems)
+}
